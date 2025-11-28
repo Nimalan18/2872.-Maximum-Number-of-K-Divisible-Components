e91c98e775bc3b30e45ebbd2eff50a405df6d506
Nimalan Arasu S <nimalanarasusenthil@gmail.com>
2025-11-28 09:40:53 +0530

    Day34.java

---
+++ Day34.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int comp = 0;
+
+    private long dfs(int node, int parent, List<List<Integer>> adj, int[] values, int k) {
+        long total = values[node]; 
+        for (int neighbor : adj.get(node)) {
+            if (neighbor != parent) {
+                total += dfs(neighbor, node, adj, values, k);
+            }
+        }
+
+        if (total % k == 0) {
+            comp += 1;
+            return 0;
+        }
+        return total % k;
+    }
+
+    public int maxKDivisibleComponents(int n, int[][] edges, int[] values, int k) {
+        List<List<Integer>> adj = new ArrayList<>(n);
+        comp = 0;
+
+        for (int i = 0; i < n; ++i)
+            adj.add(new ArrayList<>());
+
+        for (int[] e : edges) {
+            int u = e[0], v = e[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        dfs(0, -1, adj, values, k);
+        return comp;
+    }
+}
